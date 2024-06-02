90cd593f8963560ba4b45d653f2294904afd6965
Mahdi Mnx <isdil6777@gamil.com>
2024-06-02 15:38:30 +0300

    mahdi

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-
 function App() {
   return (
-   <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
-   </>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world! jacars</h1>
+      <p>changes ayaan sameeyey</p>
+    </>
+  );
 }
 
-export default App
+export default App;
