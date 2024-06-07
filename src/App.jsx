33ded2df163ef804a89a9a1cafad382e01a88986
Mahdi Mnx <isdil6777@gamil.com>
2024-06-07 19:42:19 +0300

    waxbaro aan barwaaqo gaarne

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ function App() {
     <>
       <h1 className="text-3xl font-bold underline">Hello world! jacars</h1>
       <p>changes ayaan sameeyey</p>
+      <p>hello my friendos</p>
+      <p>waxbaro moha jakaar</p>
     </>
   );
 }
